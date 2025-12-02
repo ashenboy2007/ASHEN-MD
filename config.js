e490e7255f063a80f12f722941a3e50741135e98
ashenboy2007 <hhdinuka99@gmail.com>
2025-12-02 15:54:18 +0530

    Update default SESSION_ID in config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'ASHEN-MD=atJHkb5A#EfGrV24zmb1pL3e5cd6m3L20xTrhO9Z0uJr_lBUawyQ' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASHEN-MD=wUYGxaCI#468vQKjU7nMqADWh1bxHp2aqWLROjElOba2ZYSO_rFo' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'ASHEN-MD7' : process.env.SESSION_NAME, 
 
